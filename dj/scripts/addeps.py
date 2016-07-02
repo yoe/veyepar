aa4dbfdc790e26ceebad911584865f2e647b420b
Debian veyepar user <nobody@no.where>
2016-07-02 09:19:57 +0000

    Add all fields to the .create() call where django (wrongly) believes they are non-NULL

--- dj/scripts/addeps.py
+++ dj/scripts/addeps.py
@@ -493,6 +493,9 @@ class add_eps(process.process):
                           start=row['start'],
                           duration=row['duration'],
                           name=row['name'],
+                          twitter_id=row['twitter_id'],
+                          language='',
+                          summary=row['description'],
                           )
                     episode.sequence=seq
                     episode.state=1
